bcb9c5e9aba14121b6cb87fc53a758cbf49de589
kolter <wksaurey@gmail.com>
2024-10-24 23:24:04 -0600

    finished cleaning up windows file structure

--- src/unpacker.py
+++ src/unpacker.py
@@ -1,8 +1,8 @@
-from os import path, makedirs
+import os
 from zipfile import ZipFile
 
 def get_build_name(file_location):
-    filename = path.basename(file_location)
+    filename = os.path.basename(file_location)
     name_segments = filename.split()
     build_name = [name_segments[i] for i in [0, 4, 5, 8]]
 
@@ -18,8 +18,45 @@ def get_build_name(file_location):
 def unpack(file_location):
 
     build_name = get_build_name(file_location)
-    zip_ref = ZipFile(file_location, 'r')
-    zip_ref.extract(file_location, f'bin/temp_build/{build_name}')
+    extract_path = f'bin/temp_build/{build_name}'
+    with ZipFile(file_location, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+    print(f'Extracted contents to {extract_path}')
+    convert_file_structure_from_windows(extract_path)
+
+def convert_file_structure_from_windows(dir_location):
+    # converting windows garbage
+
+    # Loop through each file in the directory
+    for filename in os.listdir(dir_location):
+        
+        if '\\' in filename:
+            # Create the new filename by replacing backslashes with forward slashes
+            new_filename = filename.replace('\\', '/')
+            
+            # Create full paths for renaming
+            old_file_path = os.path.join(dir_location, filename)
+            new_file_path = os.path.join(dir_location, new_filename)
+            
+            # build file structure
+            new_dir_path = os.path.dirname(new_file_path)
+            if not os.path.exists(new_dir_path):
+                print(f'Making dir {new_dir_path}')
+                os.makedirs(new_dir_path)
+
+            if not os.path.isdir(new_file_path):
+                # Rename the file
+                print(f'Renaming: {old_file_path} -> {new_file_path}')
+                os.rename(old_file_path, new_file_path)
+            else:
+                # delete {old_file_path
+                print(f'Deleting {old_file_path}')
+                os.remove(old_file_path)
+        else:
+            print(f'No backslash found in: {filename}')
+
+    print("All files have been processed.")
 
 if __name__ == "__main__":
     pass
